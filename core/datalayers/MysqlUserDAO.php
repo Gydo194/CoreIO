80f49a9adc4d15cd1c4edacf6a3678139dd737e1
Gydo194 <gydo.kosten@gmail.com>
2018-02-15 21:57:25 +0100

    Create PDO factory (separate from mysql user dao ) WARN: application broken (need include pdo factory in main)

--- core/datalayers/MysqlUserDAO.php
+++ core/datalayers/MysqlUserDAO.php
@@ -5,32 +5,6 @@
  * 
  * MySQL User DAO class
  */
-//no personal passwords in here today
-defined("MYSQL_USER") || define("MYSQL_USER","hibernate");
-defined("MYSQL_PASS") || define("MYSQL_PASS","hibernate");
-defined("MYSQL_HOST") || define("MYSQL_HOST","127.0.0.1");
-defined("MYSQL_DBNAME") || define("MYSQL_DBNAME","coreio");
-
-class PDOFactory {
-    private static function getPDOInstance() {
-        $db = MYSQL_DBNAME;
-        $host = MYSQL_HOST;
-        $pass = MYSQL_PASS;
-        $user = MYSQL_USER;
-        $dbc = new PDO("mysql:dbname=$db;host=$host", $user, $pass);
-        return $dbc;
-    }
-    
-    public static function query(string $sql, array $args) {
-        $db = self::getPDOInstance();
-        $stmt = $db->prepare($sql);
-        $stmt->execute($args);
-        return $stmt;
-    }
-    
-}
-
-
 
 class MysqlUserDAO implements IDAOUser {
     //put your code here
