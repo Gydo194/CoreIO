e9756e754fdf32232ee8299c96ad742c26994c67
Gydo194 <gydo.kosten@gmail.com>
2018-02-28 16:58:04 +0100

    DimLight controller update works

---
+++ web/controllers/DimLightController.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DimLightController
+ *
+ * @author gydo194
+ */
+
+defined("DIMLIGHT_REQUEST_VALUE_VARNAME") || define("DIMLIGHT_REQUEST_VALUE_VARNAME","value");
+defined("DIMLIGHT_REQUEST_PIN_VARNAME") || define("DIMLIGHT_REQUEST_PIN_VARNAME","pin");
+defined("DIMLIGHT_REQUEST_GROUP_VARNAME") || define("DIMLIGHT_REQUEST_GROUP_VARNAME","groupname");
+defined("DIMLIGHT_REQUEST_ID_VARNAME") || define("DIMLIGHT_REQUEST_ID_VARNAME","dimlightid");
+defined("DIMLIGHT_PERMISSION_NAME") || define("DIMLIGHT_PERMISSION_NAME","dimled");
+
+class DimLightController {
+    
+    private static $instance = null;
+    public static function getInstance(): DimLightController {
+        if(null === self::$instance) {
+            self::$instance = new DimLightController();
+        }
+        return self::$instance;
+    }
+    
+    private $dao = null;
+    
+    
+    
+    
+    public function __construct() {
+        $this->dao = MysqlDimLightDAO::getInstance();
+    }
+    
+    
+    
+    public function updateDimLight() {
+        
+        
+        
+        
+        if(!isset($_REQUEST[DIMLIGHT_REQUEST_ID_VARNAME])) {
+            error_log("DimLightController::updateDimLight(): missing id param");
+            return;
+        }
+        
+        $id = $_REQUEST[DIMLIGHT_REQUEST_ID_VARNAME];
+       
+         $d = $this->dao->getDimLight($id);
+        
+        if(null === $d) {
+            error_log("DimLightController::updateDimLight(): cannot get dimlight object from dao, is id correct?");
+            return;
+        }
+        
+        
+        
+        
+        
+        if(isset($_REQUEST[DIMLIGHT_REQUEST_VALUE_VARNAME])) {
+            //update the value
+            $value = intval($_REQUEST[DIMLIGHT_REQUEST_VALUE_VARNAME]);
+            $d->setValue($value);
+        }
+        
+        if(isset($_REQUEST[DIMLIGHT_REQUEST_PIN_VARNAME])) {
+            $pin = intval($_REQUEST[DIMLIGHT_REQUEST_PIN_VARNAME]);
+            $d->setPin($pin);
+        }
+        
+        if(isset($_REQUEST[DIMLIGHT_REQUEST_GROUP_VARNAME])) {
+            $groupname = strval($_REQUEST[DIMLIGHT_REQUEST_GROUP_VARNAME]);
+            $d->setGroup($groupname);
+        }
+        
+        
+        
+        $this->dao->updateDimLight($d);
+       
+        
+        
+        
+        
+        
+    }
+}
